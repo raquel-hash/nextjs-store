fd0a161beeebabbc9fb71cdf07933222f6672273
Raquel <quelcaraquel1@gmail.com>
2024-06-05 17:57:05 -0400

    Cómo agregar archivos estáticos en Next.js

--- src/Components/home/Description/Description.tsx
+++ src/Components/home/Description/Description.tsx
@@ -1,7 +1,14 @@
 export const Desccription = () => {
   return (
     <section>
-      <h1>Descripcion</h1>
+      <img src="/images/description.jpeg" alt="prodcuts marketplace" />
+      <h2>Descripcion</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        minus delectus ullam voluptatem aliquid amet exercitationem recusandae
+        sequi commodi tenetur, modi in fugiat corporis repellendus, odio
+        eligendi laudantium similique quae?
+      </p>
     </section>
   );
 };
