cebea3ef41931465ba97c0b0b23f44fbb8bf766f
Raquel <quelcaraquel1@gmail.com>
2024-11-12 17:42:39 -0400

    Proyecto: implementando la página de tienda

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,13 +1,18 @@
+import { ProductsWrapper } from "app/Components/Store/ProductsWrapper";
+import { getProducts } from "app/services/shopify";
+
 interface CategoryProps {
   params: {
     categories: string[];
     searchParams?: string;
   };
 }
-export default function Category(props: CategoryProps) {
-  console.log(props);
+export default async function Category(props: CategoryProps) {
+  const products = await getProducts();
+  // console.log(props);
   const { categories } = props.params;
   // throw new Error("Error: Boom!");
-  console.log(categories);
-  return <h1>Categoria dinamica: {categories} </h1>;
+  // console.log(categories);
+  // return <h1>Categoria dinamica: {categories} </h1>;
+  return <ProductsWrapper products={products} />;
 }
