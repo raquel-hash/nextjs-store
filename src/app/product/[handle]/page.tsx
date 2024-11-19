49706e5aeb9b2116b3fcfb316c579e1f5f410105
Raquel <quelcaraquel1@gmail.com>
2024-11-19 09:32:53 -0400

    Data fetching de parámetros en el servidor y cliente

---
+++ src/app/product/[handle]/page.tsx
@@ -0,0 +1,16 @@
+// interface ProductPageProps {
+//     searchParams: {
+//         id: string
+//     }
+// }
+"use client";
+
+import { useParams, useSearchParams } from "next/navigation";
+
+export default function ProductPage() {
+  const params = useParams();
+  const searchParams = useSearchParams();
+
+  const id = searchParams.get("id");
+  return <h1>Product Page</h1>;
+}
