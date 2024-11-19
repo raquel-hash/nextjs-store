5e39d2ca60670dce1ea9f6e75af16e7615e04539
Raquel <quelcaraquel1@gmail.com>
2024-11-19 11:39:19 -0400

    Mejorando SEO de una página en Next.js

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,13 @@
 import { Description } from "app/Components/home/Description";
 import { MainProducts } from "app/Components/home/MainProducts";
 import { Hero } from "app/Components/home/Hero";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Future World",
+  description: "Welcome to the future world, an ecommerce from other century",
+  keywords: ["ecommerce", "future", "world", "tecnnology"],
+};
 
 export default function Home() {
   return (
